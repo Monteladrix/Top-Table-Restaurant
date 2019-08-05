3295aef745e3ccb5e937838dcc0dcd63e81e297f
User <lechantallastimosa@gmail.com>
2019-08-05 21:45:41 +0800

    hello world

---
+++ app/src/main/java/com/example/toptablerestaurant/Common/Common.java
@@ -0,0 +1,7 @@
+package com.example.toptablerestaurant.Common;
+
+import com.example.toptablerestaurant.Model.User;
+
+public class Common {
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/example/toptablerestaurant/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.toptablerestaurant;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn;
+    TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(), "font/Nabila.ttf");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this, SignIn.class);
+                startActivity(signIn);
+            }
+        });
+    }
+}
